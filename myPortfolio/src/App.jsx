a50cf35a6e83df44af8c3dfe52e2b5dafe2777ca
IndhiraKumaaran <indhirakumaaran3@gmail.com>
2025-05-30 13:09:37 +0530

    once again

--- myPortfolio/src/App.jsx
+++ myPortfolio/src/App.jsx
@@ -1,33 +1,30 @@
-import { useState, useEffect } from 'react'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import RootLayout from './Layout/RootLayout'
 import Home from './Pages/Home'
 import Skills from './Pages/Skills'
 import Projects from './Pages/Projects'
 import Contact from './Pages/Contact'
 import LoadingScreen from './Pages/LoadingScreen'
+import { useState } from 'react'
+
 function App() {
   const [loading, setLoading] = useState(true)
-
- const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route index element={<Home />} />
-      <Route path="Skills" element={<Skills />} />
-      <Route path="Projects" element={<Projects />} />
-      <Route path="Contact" element={<Contact />} />
-    </Route>
-  ),
-  { basename: '/Portfolio-1' }  // <-- add this option here
-)
-
 
   return (
     <>
       {loading ? (
         <LoadingScreen onFinish={() => setLoading(false)} />
       ) : (
-        <RouterProvider router={router} />
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<RootLayout />}>
+              <Route index element={<Home />} />
+              <Route path="Skills" element={<Skills />} />
+              <Route path="Projects" element={<Projects />} />
+              <Route path="Contact" element={<Contact />} />
+            </Route>
+          </Routes>
+        </HashRouter>
       )}
     </>
   )
